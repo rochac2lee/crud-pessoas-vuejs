752444a8279b783aa2a4243b64ebec943add69ee
rochac2lee <cleberli.rocha@gmail.com>
2020-10-20 21:04:01 -0300

    Estilização da página Home

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,5 +1,26 @@
-<div class="head">
-    <h1>Cadastro de Pessoas</h1>
+<div class="bg"></div>
 
-    
-</div>
+<main class="main">
+    <div class="head">
+        <h1>Cadastro de Pessoas</h1>
+
+        <div class="content">
+
+            <table class="table">
+                <thead class="thead">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Data de Nascimento</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+
+        </div>
+
+    </div>
+</main>
